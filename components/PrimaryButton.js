c1f9874c2deae66ce48324856b1470d3add68fdc
avinashverma <avinash.verma@myn.global>
2024-12-06 15:26:38 +0530

    added screens and button component

---
+++ components/PrimaryButton.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function PrimaryButton({ chirdren }) {
+  return (
+    <View>
+      <Text>{chirdren}</Text>
+    </View>
+  );
+}
+
+export default PrimaryButton;
